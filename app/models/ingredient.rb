832f17e09b99841572da5707cac25bd7d6d8a48f
JulJen <juliajen.prog@gmail.com>
2018-05-15 12:24:12 -0400

    Update model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,9 @@
 class Ingredient < ActiveRecord::Base
 
   has_many :recipe_ingredients
-  has many :recipes, through: :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
   has_many :users, through: :recipes
   # CRUD
   # If the table has a name column, it generates name, and name=
   # Save the item
+end
