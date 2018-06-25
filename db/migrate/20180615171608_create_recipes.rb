d139fa8d7aae70b525db3237298558a46dda58fa
JulJen <juliajen.prog@gmail.com>
2018-06-25 14:27:34 -0400

    Update tables

--- db/migrate/20180615171608_create_recipes.rb
+++ db/migrate/20180615171608_create_recipes.rb
@@ -2,7 +2,6 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :content
       t.integer :user_id
     end
   end
--- db/migrate/20180615171614_create_ingredients.rb
+++ db/migrate/20180615171614_create_ingredients.rb
@@ -2,8 +2,8 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.string :season
       t.integer :recipe_id
+      t.integer :user_id
     end
   end
 end
