870121e26febf81826e8fc8dff507e5f544a9a6d
JulJen <juliajen.prog@gmail.com>
2018-06-29 13:18:17 -0400

    Update db tables

--- db/migrate/20180628214249_create_users.rb
+++ db/migrate/20180628214249_create_users.rb
@@ -1,12 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :fname
+      t.string :lname
       t.string :username
-      t.string :name
       t.string :email
+      t.string :password_digest
       t.string :state
       t.string :bio
-      t.string :password_digest
     end
   end
 end
--- db/migrate/20180628214316_create_ingredients.rb
+++ db/migrate/20180628214316_create_ingredients.rb
@@ -2,6 +2,8 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
       t.string :name
+      t.integer :user_id
+      t.integer :recipe_id
     end
   end
 end
