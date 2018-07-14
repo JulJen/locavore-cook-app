7ec137beb33c26b9a438176588375017be755550
JulJen <juliajen.prog@gmail.com>
2018-07-13 21:56:49 -0400

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_171614) do
+ActiveRecord::Schema.define(version: 2018_06_29_191730) do
 
   create_table "ingredients", force: :cascade do |t|
-    t.string "name"
-    t.string "season"
+    t.string "name", limit: 20
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.string "name", limit: 20
+    t.string "quantity", limit: 20
+    t.integer "ingredient_id"
     t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.string "content"
+    t.string "name", limit: 20
+    t.string "content", limit: 60
+    t.string "directions", limit: 250
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "name"
+    t.string "fname", limit: 20
+    t.string "lname", limit: 20
+    t.string "username", limit: 10
     t.string "email"
-    t.string "state"
-    t.string "content"
-    t.string "password_digest"
+    t.string "password_digest", limit: 10
+    t.string "state", limit: 60
+    t.string "bio", limit: 20
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
